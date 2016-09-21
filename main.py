fdddba5a6777fa6fa02eba7cf1a00e1c521fc6c8
Devilmat <mathieu.gonzales64@gmail.com>
2016-09-21 16:48:10 +0200

    recuperation id + tous les prix depuis un pcap

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from scapy.all import *
 
 from recuperer_infos_paquets import recuperer_infos_paquets
 
-print "Premier pcap"
-liste1 = recuperer_infos_paquets("ailes_cassees.pcap")
-print "\n\nDeuxieme pcap"
-liste2 = recuperer_infos_paquets("ailes_moskito.pcap")
+(id_objet, prix_objet) = recuperer_infos_paquets("ailes_moskito.pcap")
 
-for i in range(0, len(liste1)):
-	print "Paquet",i
-	if liste1[i].haslayer(Raw):
-		print liste1[i].getlayer(Raw).load.encode('hex')
-	else:
-		print "Pas de Raw"
-	if liste2[i].haslayer(Raw):
-		print liste2[i].getlayer(Raw).load.encode('hex')
-	else:
-		print "Pas de Raw"
+
--- recuperer_infos_paquets.py
+++ recuperer_infos_paquets.py
@@ -9,12 +9,31 @@ def recuperer_infos_paquets(fichier_pcap):
 
 	liste_paquets=rdpcap(fichier_pcap)
 	i = 0
+	id_objet = ""
+	prix_objet = []
+	#les paquets commencent a 0
 	for p in liste_paquets:
-		print "Paquet", i
+		data = ""
+
+		#print "Paquet", i
+
+		#si le paquet contient un data
 		if p.haslayer(Raw):
-			print p.getlayer(Raw).load.encode('hex')
-		else:
-			print "Pas de Raw"
+			data = p.getlayer(Raw).load.encode('hex')
+			#print data
+
+			#paquet contenant l'ID du l'objet (paquet no 2)
+			if "5abd02" in data:
+				id_objet = data[6:]
+				print "ID objet :", id_objet
+
+			#paquet contenant les prix de l'objet (paquet no 3)
+			if "636904a" in data and len(data)>50:
+				prix_objet.append(str(int(data[38:46], 16)) + 'k')
+				prix_objet.append(str(int(data[46:54], 16)) + 'k')
+				prix_objet.append(str(int(data[54:62], 16)) + 'k')
+				print "Prix :", prix_objet
+
 		i = i+1
 
-	return liste_paquets
+	return (id_objet, prix_objet)
